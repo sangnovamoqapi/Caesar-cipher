991d5ac28cf81015a4707fe2b617084384bec3d0
Jigar Morjaria <56693923+sangnovamoqapi@users.noreply.github.com>
2020-02-20 00:53:30 +0530

    Encrypting Data int a file
    In case of Testing main.c and in absence of the test data, this file can be used for shifting letters(Shift Cipher).

---
+++ encrypt.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+     FILE *fp1,*fp2,*fp3;
+     char c,encryptwith;
+     fp1=fopen("file.txt","r");
+     fp2=fopen("file4.txt","w");
+     fp3 = fopen("file2.txt","w");
+    while((c=fgetc(fp1))!=EOF){
+        fputc(tolower(c),fp3);
+    }
+    fclose(fp3);
+    printf("ENCRYPTWITH:\n");
+    scanf("%d",&encryptwith);
+    fp3 = fopen("file2.txt","r");
+    while((c=fgetc(fp3))!=EOF){
+         if(c==' '){
+             fputc(c,fp2);
+             continue;
+         }
+         if(c=='\n'){
+             fputc(c,fp2);
+             continue;
+         }
+         if(c=='\t'){
+             fputc(c,fp2);
+             continue;
+         }
+         if(c<65 || c > 122){
+             fputc(c,fp2);
+             continue;
+         }
+        c-=encryptwith;
+        if(c>122 && c<122 + encryptwith +1)
+            c-=26;
+        if(c<97  && c> 97 - encryptwith -1)
+            c+=26;
+        //char[i]=c;
+        fputc(c,fp2);
+     }
+     //while(char[i]!=EOF){
+         
+     //}
+     fclose(fp1);
+     fclose(fp2);
+     fclose(fp3);
+     return 0;
+}
